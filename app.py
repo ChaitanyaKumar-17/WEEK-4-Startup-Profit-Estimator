83a76ceadcd660009a85df2d8c8665d2f0656e19
Chaitanya Kumar <chaitanya0817@gmail.com>
2026-05-11 15:34:41 +0530

    did the visual analysis

--- app.py
+++ app.py
@@ -25,12 +25,12 @@ df = pd.concat([df, encoded_df], axis = 1)
 df.drop(['State'], axis = 1, inplace = True)
 print(df.head())
 
-# Now let's split the test and training data
+# Spliting the Test and Training Data
 y = df['Profit']
 X = df.drop(['Profit'], axis = 1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Let's visualize the training dataset
+# visualizing the training dataset
 sns.set_theme(style="whitegrid")
 fig, axes = plt.subplots(1, 3, figsize=(18, 5))
 
@@ -49,17 +49,17 @@ axes[2].set_title('Marketing Spend vs Profit')
 plt.tight_layout()
 plt.show()
 
-# Now let's do the cross-validation of the training dataset 
+# Cross-validation of the training dataset 
 model = LinearRegression()
 
 cv_scores = cross_val_score(model, X_train, y_train, cv=5)
 print(f"Cross-Validation Scores: {cv_scores}")
 print(f"Average CV Score: {np.mean(cv_scores):.4f}")
 
-# Now let's fit the model
+# Fiting the model
 model.fit(X_train, y_train)
 
-# Now let's do testing of the trained model along with their R2 score 
+# Testing of the trained model along with their R2 scores
 y_train_pred_lr = model.predict(X_train)
 y_test_pred_lr = model.predict(X_test)
 
@@ -68,7 +68,7 @@ print(f"Train R²: {r2_score(y_train, y_train_pred_lr):.4f} | Test R²: {r2_scor
 print(f"Train RMSE: {np.sqrt(mean_squared_error(y_train, y_train_pred_lr)):.2f} | Test RMSE: {np.sqrt(mean_squared_error(y_test, y_test_pred_lr)):.2f}")
 
 
-# L1 nad L2 Regularization
+# L1 and L2 Regularization
 ridge_model = Ridge(alpha=10.0)
 lasso_model = Lasso(alpha=100.0)
 
@@ -99,23 +99,21 @@ coef_df = pd.DataFrame({
 # Sort by the absolute impact on profit
 print(coef_df.sort_values(by='Coefficient', ascending=False, key=abs))
 
-# # ==========================================
-# # 4. Plot Predicted vs Actual & Residuals
-# # ==========================================
-# fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+# Visual Analysis
+fig, axes = plt.subplots(1, 2, figsize=(15, 6))
 
-# # Plot 1: Actual vs Predicted Profits (using MLR model)
-# axes[0].scatter(y_test, y_test_pred_lr, color='teal', alpha=0.7)
-# axes[0].plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2) # Line of perfect prediction
-# axes[0].set_xlabel('Actual Profit')
-# axes[0].set_ylabel('Predicted Profit')
-# axes[0].set_title('Actual vs Predicted Profit (MLR)')
+# Plot 1: Actual vs Predicted Profits (using MLR model)
+axes[0].scatter(y_test, y_test_pred_lr, color='teal', alpha=0.7)
+axes[0].plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2) # Line of perfect prediction
+axes[0].set_xlabel('Actual Profit')
+axes[0].set_ylabel('Predicted Profit')
+axes[0].set_title('Actual vs Predicted Profit (MLR)')
 
-# # Plot 2: Residuals Distribution
-# residuals = y_test - y_test_pred_lr
-# sns.histplot(residuals, kde=True, ax=axes[1], color='indigo')
-# axes[1].set_xlabel('Residuals (Actual - Predicted)')
-# axes[1].set_title('Distribution of Residuals')
+# Plot 2: Residuals Distribution
+residuals = y_test - y_test_pred_lr
+sns.histplot(residuals, kde=True, ax=axes[1], color='indigo')
+axes[1].set_xlabel('Residuals (Actual - Predicted)')
+axes[1].set_title('Distribution of Residuals')
 
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.show()
